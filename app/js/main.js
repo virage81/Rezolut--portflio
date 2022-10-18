feddd6d120bdb1475f355fd256ac7a2800ce800b
soret <karimovminds@gmail.com>
2022-10-18 21:18:42 +0400

    i've done header and footer, also i've done animated fixed header FINALLY

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function () {
-	
-});
+function headerFixed() {
+	let header = document.querySelector("#header"),
+		scrollTop = 0,
+		headerHeight = header.offsetHeight + header.offsetTop;
+
+	window.onscroll = () => {
+		scrollTop = window.scrollY;
+
+		scrollTop >= headerHeight ? header.classList.add("header--fixed") : header.classList.remove("header--fixed");
+	};
+}
+
+headerFixed();
