51ecac4627c3af72e7cf8797a299f1c209c1ad9b
soret <karimovminds@gmail.com>
2022-10-22 17:21:19 +0400

    added adaptive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,5 @@
 $(function () {
-	function headerFixed() {
-		let header = document.querySelector("#header"),
-			scrollTop = 0,
-			headerHeight = header.offsetHeight + header.offsetTop;
-
-		window.onscroll = () => {
-			scrollTop = window.scrollY;
-
-			scrollTop >= headerHeight ? header.classList.add("header--fixed") : header.classList.remove("header--fixed");
-		};
-	}
-
-	headerFixed();
-
+	// Slider
 	let reviewsContainer = $("#slider");
 
 	reviewsContainer.slick({
@@ -20,3 +7,38 @@ $(function () {
 		fade: true,
 	});
 });
+
+// fixed Header
+let header = document.querySelector("#header"),
+	headerHeight = parseInt(header.offsetHeight + header.offsetTop),
+	headerBtn = document.querySelector("#header-button"),
+	headerNav = document.querySelector("#header-nav"),
+	scrollTop = 0;
+
+function checkPos(e) {
+	scrollTop = Math.floor(e.currentTarget.pageYOffset);
+	if (scrollTop >= headerHeight) {
+		header.classList.add("header--fixed");
+	} else {
+		header.classList.remove("header--fixed");
+	}
+	headerNav.classList.remove("show");
+	header.classList.remove("header--show-nav");
+}
+
+window.addEventListener("scroll", checkPos);
+window.addEventListener("load", checkPos);
+
+// checkPos();
+
+// adaptive Header
+headerBtn.onclick = () => {
+	headerNav.classList.toggle("show");
+	headerNav.classList.contains("show") ? header.classList.add("header--show-nav") : header.classList.remove("header--show-nav");
+};
+
+window.onclick = closeHeader;
+
+function closeHeader(e) {
+	if (e.target) console.log(e.target);
+}
